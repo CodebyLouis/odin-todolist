8598a4fdaf860915953dc8ee9fa10cf1827ff6cd
Frozbee <louisrouyer65@gmail.com>
2025-12-01 18:49:03 +0100

    Initial setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Nettoie dist avant chaque build
+  },
+
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Ton HTML de base
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
